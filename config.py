352b4f32d443b849d2524f1adc5b83199753820c
Шайхутдинов Руслан <57287494+Violonistx@users.noreply.github.com>
2025-05-18 18:11:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Получаем токен бота из переменных окружения
-BOT_TOKEN = "7815643193:AAExrmaoLEBynnKfoD2RHwLjNmFSwFcicj0"
-
+BOT_TOKEN = #############
 if not BOT_TOKEN:
     raise ValueError("Не найден токен бота. Убедитесь, что файл .env существует и содержит BOT_TOKEN")
